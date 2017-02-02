048b4dcd1b077a0cc8098988666e729f0fcd30d5
Benjamin Ayles <ben@ayles.com.au>
2017-02-02 23:06:15 +1030

    maybe like that?

--- PiPool/pin_controller.py
+++ PiPool/pin_controller.py
@@ -6,7 +6,7 @@ class PinController(object):
     def __init__(self):
         RPIO.setmode(RPIO.BCM)
 
-        self.my_pins = Pin.objects
+        self.my_pins = None
         self.set_all_pins()
 
     def get_thermometers(self):
@@ -15,13 +15,18 @@ class PinController(object):
     def set_all_pins(self):
         RPIO.cleanup()
 
-        for pin in Pin.objects.all():
+        pins = Pin.objects.all()
+
+        for pin in pins:
             try:
                 RPIO.setup(pin.pin_number, pin.get_direction())
                 RPIO.output(pin.pin_number, RPIO.HIGH)
 
-                self.my_pins.add(pin)
-            except: pass
+                # self.my_pins.add(pin)
+            except:
+                pins.remove(pin)
+
+        self.my_pins = pins
 
     def get_dashboard_data(self):
         data = {'thermometers': self.get_thermometers(), 'pins': self.my_pins.filter(is_thermometer=False)}
