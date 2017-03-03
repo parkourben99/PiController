446e797d18cfe48b1296eb52af2de772fda9250a
Benjamin Ayles <ben@ayles.com.au>
2017-03-03 21:56:13 +1030

    create command

---
+++ PiControl/management/commands/tempcontrol.py
@@ -0,0 +1,36 @@
+from django.core.management.base import BaseCommand, CommandError
+from PiControl.models import Pin
+
+
+class TempControl(BaseCommand):
+    help = 'Maintain the temp for the pool'
+    temp = 23
+
+    def handle(self, *args, **options):
+
+        self.stdout.write(self.style.SUCCESS('Successfully closed poll'))
+
+
+# Rasberry Pi Spa Pool Controller
+#
+# Outputs to control:
+# Relay Switching: Pump
+# Relay Switching: Heater
+#
+# Inputs to Control
+# Water Temp Thermometer
+# Water Flow sensor (This is optional as its used so the heater cannot be turned on with out water flow from pump)
+        # This can be done with a simple relay in the heater circut to take it out of the Rasberry Pi
+#
+#
+# Web Login or App to control Set Temp of pool, Pump on/off, display current Water Temp
+# Touch screen Control at the pool of these same settings plus * Settings
+#
+# The way the spa should operate is that it requires a minimum run time per 24hrs ie 2* hours (To allow for Filtering)
+# This can but doesnt have to include time where the spa is set manually on (To have running while I sit in it)
+# It needs to be able to automatically keep itself within a 2.5* degree range of a set temperature (automatically turning the pump and heater on and off)
+# It needs to have Time bands* that it operates (When outside set Time bands the Pool doesnt need to filter or heat
+# A Manual ON mode where the Pump runs but the Heater only runs as required
+#
+#
+# *= adjustble through GUI on Touch screen
