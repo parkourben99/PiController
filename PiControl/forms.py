6ae4c97c620658ab6b34151a45a958b7364afcde
Benjamin Ayles <ben@ayles.com.au>
2017-04-30 17:04:38 +0930

    validation serverside

--- PiControl/forms.py
+++ PiControl/forms.py
@@ -32,12 +32,32 @@ class TimeBandForm(forms.ModelForm):
     day_of_week = forms.ChoiceField(choices=((0, 'Monday'), (1, 'Tuesday'), (2, 'Wednesday'), (3, 'Thursday'), (4, 'Friday'), (5, 'Saturday'), (6, 'Sunday')))
     active = forms.BooleanField(initial=True, required=False)
 
+    # def clean_start_at(self):
+    #     start_at = self.cleaned_data['start_at']
+    #
+    #     time = datetime.strptime('%I:%M %p', start_at)
+    #     time = time.strftime("%H:%M")
+    #
+    #     return time
+
+    def clean_send_at(self):
+        start = self.cleaned_data['start_at']
+        end = self.cleaned_data['end_at']
+
+        start_at = datetime.strptime('%H:%M', start)
+        end_at = datetime.strptime('%H:%M', end)
+
+        if start_at >= end_at:
+            raise forms.ValidationError(u'The end time must be after the start time')
+
+        return end
+
     class Meta:
         model = TimeBand
         fields = ('start_at', 'end_at', 'active', 'day_of_week', 'id')
         widgets = {
-            'start_at': forms.TextInput(attrs={'class': 'form-control js-start-at'}),
-            'end_at': forms.TextInput(attrs={'class': 'form-control js-end-at'}),
+            'start_at': forms.TimeField(attrs={'class': 'form-control js-start-at'}),
+            'end_at': forms.TimeField(attrs={'class': 'form-control js-end-at'}),
             'day_of_week': forms.Select(attrs={'class': 'form-control'}),
-            'active': forms.CheckboxInput(attrs={'class': 'form-control'}, ),
+            'active': forms.CheckboxInput(attrs={'class': 'form-control'}),
         }
