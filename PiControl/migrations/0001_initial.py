bf3d080a9b7979dd098b9c10bb2aef94c5383d48
Benjamin Ayles <ben@ayles.com.au>
2017-08-12 23:35:08 +0930

    muppet not tabed

--- PiControl/migrations/0001_initial.py
+++ PiControl/migrations/0001_initial.py
@@ -10,39 +10,39 @@ class Migration(migrations.Migration):
     initial = True
     dependencies = []
 
-operations = [
-    migrations.CreateModel(
-        name='Pin',
-        fields=[
-            ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-            ('name', models.CharField(max_length=200)),
-            ('description', models.CharField(max_length=200)),
-            ('pin_number', models.IntegerField()),
-            ('is_thermometer', models.BooleanField(default=False)),
-            ('updated_at', models.DateTimeField(auto_now=True)),
-            ('created_at', models.DateTimeField(auto_now_add=True)),
-        ],
-    ),
-    migrations.CreateModel(
-        name='Schedule',
-        fields=[
-            ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-            ('start_at', models.TimeField(null=False)),
-            ('end_at', models.TimeField(null=False)),
-            ('day_of_week', models.IntegerField(null=False)),
-            ('active', models.BooleanField(default=True)),
-            ('pin_id', models.IntegerField()),
-            ('updated_at', models.DateTimeField(auto_now=True)),
-            ('created_at', models.DateTimeField(auto_now_add=True)),
-        ],
-    ),
-    migrations.CreateModel(
-        name='ScheduleHistory',
-        fields=[
-            ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-            ('schedule_id', models.IntegerField()),
-            ('updated_at', models.DateTimeField(auto_now=True)),
-            ('created_at', models.DateTimeField(auto_now_add=True)),
-        ],
-    ),
-]
+    operations = [
+        migrations.CreateModel(
+            name='Pin',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=200)),
+                ('description', models.CharField(max_length=200)),
+                ('pin_number', models.IntegerField()),
+                ('is_thermometer', models.BooleanField(default=False)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Schedule',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('start_at', models.TimeField(null=False)),
+                ('end_at', models.TimeField(null=False)),
+                ('day_of_week', models.IntegerField(null=False)),
+                ('active', models.BooleanField(default=True)),
+                ('pin_id', models.IntegerField()),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='ScheduleHistory',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('schedule_id', models.IntegerField()),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+        ),
+    ]
